6242aab0295cd060ead71ffe0a34298c63968065
wa008 <2864436211@qq.com>
2019-03-31 14:53:05 +0800

    update number

--- leetcode/leetcode_1014.py
+++ leetcode/leetcode_1014.py
@@ -1,32 +1,9 @@
 class Solution:
-    def shipWithinDays(self, weights, D) -> int:
-        n = len(weights)
-        def check(x):
-            now = 0
-            num = 1
-            nonlocal n
-            nonlocal weights
-            nonlocal D
-            for i in range(n):
-                if now + weights[i] > x:
-                    now = weights[i]
-                    num += 1
-                else:
-                    now += weights[i]
-            if num > D:
-                return False
-            else:
-                return True
-
-        l = 1
-        for i in range(n):
-            l = max(l, weights[i])
-        r = 25000000
-        while l < r:
-            mid = (l + r) >> 1
-            if check(mid):
-                r = mid
-            else:
-                l = mid + 1
-        return r
-
+    def maxScoreSightseeingPair(self, A: List[int]) -> int:
+        n = len(A)
+        now = A[0]
+        ans = -100000
+        for i in range(1,n):
+            ans = max(ans, now+A[i]-1)
+            now = max(now-1, A[i])
+        return ans
--- leetcode/leetcode_1021.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxScoreSightseeingPair(self, A: List[int]) -> int:
-        n = len(A)
-        now = A[0]
-        ans = -100000
-        for i in range(1,n):
-            ans = max(ans, now+A[i]-1)
-            now = max(now-1, A[i])
-        return ans
