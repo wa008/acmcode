2437097e7e5e36820f0c1e8db148d5b6049a7950
wa007 <2864436211@qq.com>
2019-08-21 19:58:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import math
 
 print(math.gcd(12,16))
+https://taskpf.baidu.com/activity/h5/carveupcoinshare?productid=1&inviteCode=KBRDVF4&autodialog=jl-db-gc-gh&pkg=[pkg]&ts=8MxdqkydwkYmRWamJ81ya_MxX9NK4b9CYM2A8gPS2fgqae_ZA&vr=2&tab=&tag=&source=&pd=&idfrom=share_haokan_qq_indexhttps://taskpf.baidu.com/activity/h5/carveupcoinshare?productid=1&inviteCode=KBRDVF4&autodialog=jl-db-gc-gh&pkg=[pkg]&ts=8MxdqkydwkYmRWamJ81ya_MxX9NK4b9CYM2A8gPS2fgqae_ZA&vr=2&tab=&tag=&source=&pd=&idfrom=share_haokan_qq_index
